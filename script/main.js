d97665f60cf7be561ea8ec5f1fbb446d2265e97b
jaakko <jaakko.st.karhu@gmail.com>
2015-04-22 14:50:42 +0300

    button stopperi laitettu, vois vielä viilaa

--- script/main.js
+++ script/main.js
@@ -1,8 +1,9 @@
-$(document).ready(function() {
+var direction ='';
+var degree = '';
+var easing = '';
+var flipCount = 0;
 
-  var direction ='';
-  var degree = '';
-  var easing = '';
+$(document).ready(function() {
 
   $('.karhu.flip-card.holder').each(function(index) {
 
@@ -17,20 +18,29 @@ $(document).ready(function() {
 
   $('.karhu.flip-card.btn').on('click', function() {
 
+    if ($(this).hasClass('preventClick')) {
+      console.log('clickPrevented');
+      return;
+    };
+
+    console.log('does reach?');
+
     if ($(this).hasClass('next')) {
 
       direction = 'next';
       degree = '450deg';
-      $('.current').next('.card').addClass('toBeAnimated');
+      $('.current').next('.card').addClass('revealed');
 
     }
     else if ($(this).hasClass('prev')) {
 
       direction = 'prev';
       degree = '270deg';
-      $('.current').prev('.card').addClass('toBeAnimated');
+      $('.current').prev('.card').addClass('revealed');
     }
+
     findFlippinCards(direction, degree);
+    $('.karhu.flip-card.btn').addClass('preventClick');
   });
 
   $('.karhu.flip-card.holder .card').bind( 'transitionend', function() { transitionEndEvent(this); });
@@ -39,12 +49,15 @@ $(document).ready(function() {
 function findFlippinCards(direction, deg) {
 
   var timeout = 0;
+  flipCount = 0;
   $('.karhu.flip-card.holder').each(function(index) {
 
     var target = $(this).find('.current')
     setTimeout(function(){ flipCard(direction, target, deg, 'ease-in')}, timeout);
     timeout = timeout + 100;
   });
+
+  console.log(timeout);
 };
 
 function flipCard(direction, target, deg, easing) {
@@ -53,11 +66,8 @@ function flipCard(direction, target, deg, easing) {
 
     '-webkit-transform':'rotateY(' + deg + ')',
     '-ms-transform':'rotateY(' + deg + ')',
-    'transform':'perspective(500px) rotateY(' + deg + ')',
-    '-webkit-transition-transition' : '0.5s ' + easing,
-    '-ms-transition' : '0.5s ' + easing,
-    'transition' : '0.5s ' + easing
-  });
+    'transform':'perspective(500px) rotateY(' + deg + ')'//,
+  }); 
 }
 
 function transitionEndEvent(_this) {
@@ -65,16 +75,23 @@ function transitionEndEvent(_this) {
   if ($(_this).hasClass('next')) {
 
     $(_this).removeClass('next').removeClass('current');
-    flipCard( 'next', $(_this).next('.toBeAnimated'), '360deg', 'ease-out');
+    flipCard( 'next', $(_this).next('.revealed'), '360deg', 'ease-out');
   }
   else if ($(_this).hasClass('prev')) {
 
     $(_this).removeClass('prev').removeClass('current');
-    flipCard('prev', $(_this).prev('.toBeAnimated'), '360deg', 'ease-out');
+    flipCard('prev', $(_this).prev('.revealed'), '360deg', 'ease-out');
   }
 
-  if($(_this).hasClass('toBeAnimated')) {
+  if($(_this).hasClass('revealed')) { 
 
-    $(_this).removeClass('toBeAnimated').addClass('current');
+    $(_this).removeClass('revealed').addClass('current');
+    flipCount = flipCount + 1;
+  }
+
+  if(flipCount == $('.karhu.flip-card.holder').length) {
+
+    console.log('tilili');
+    $('.karhu.flip-card.preventClick').removeClass('preventClick');
   }
 }
