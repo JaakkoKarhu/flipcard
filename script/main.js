e978bacad797acfa837c1ec60da1a035c782bee8
jaakko <jaakko.st.karhu@gmail.com>
2015-04-23 12:50:20 +0300

    Eka stable

--- script/main.js
+++ script/main.js
@@ -3,24 +3,31 @@ var degree = '';
 var easing = '';
 var flipCount = 0;
 var maxLength = 0;
+var isIeNine = false;
+var timeout = 0;
 
 $(document).ready(function() {
 
-  // Onit flipcards
+  if($('html').hasClass('ie9')) { isIeNine = true; };
+
+  // Init flipcards
+  initCards();
+
+  if (isIeNine) { initIeNine(); }
+  else          { initModernBrowsers(); }
+
+  setButtons();
+  // Init end
+});
+
+function initCards() {
+
   $('.karhu.flip-card.holder').each(function(index) {
 
     if ($(this).find('.card').length > maxLength) {
 
       maxLength = $(this).find('.card').length;
     };
-
-    $(this).find('.card:first-child').css({
-
-      '-webkit-transform':'rotateY(360deg)',
-      '-moz-transform':'rotateY(360deg)',
-      '-ms-transform':'rotateY(360deg)',
-      '-o-transform':'rotateY(360deg)',
-      'transform:':'perspective(500px) rotateY(270deg)'}).addClass('current');
   });
 
   $('.karhu.flip-card.holder').each(function(index) {
@@ -34,10 +41,27 @@ $(document).ready(function() {
       }
     };
   });
+};
 
-  setButtons();
+function setButtons() {
 
-  // Init end
+  $('.karhu.flip-card.btn.next, .karhu.flip-card.btn.prev').removeClass('hide');
+  if ($('.karhu.flip-card .current').next('.card').length == 0) { ($('.karhu.flip-card.btn.next').addClass('hide')); }
+  else if ($('.karhu.flip-card .current').prev('.card').length == 0) { ($('.karhu.flip-card.btn.prev').addClass('hide')); }
+}
+
+function initModernBrowsers() {
+
+  $('.karhu.flip-card.holder').each(function(index) {
+
+    $(this).find('.card:first-child').css({
+
+      '-webkit-transform':'rotateY(360deg)',
+      '-moz-transform':'rotateY(360deg)',
+      '-ms-transform':'rotateY(360deg)',
+      '-o-transform':'rotateY(360deg)',
+      'transform:':'perspective(500px) rotateY(270deg)'}).addClass('current');
+  });
 
   $('.karhu.flip-card.btn').on('click', function() {
 
@@ -50,7 +74,6 @@ $(document).ready(function() {
       direction = 'next';
       degree = '450deg';
       $('.current').next('.card').addClass('revealed');
-
     }
     else if ($(this).hasClass('prev')) {
 
@@ -63,17 +86,16 @@ $(document).ready(function() {
     $('.karhu.flip-card.btn').addClass('preventClick');
   });
 
-  $('.karhu.flip-card.holder .card').bind( 'transitionend', function() { transitionEndEvent(this); });
-});
+   $('.karhu.flip-card.holder .card').bind( 'transitionend', function() { flipTransitionEndEvent(this); });
+};
 
 function findFlippinCards(direction, deg) {
 
-  var timeout = 0;
+  timeout = 0;
   flipCount = 0;
   $('.karhu.flip-card.holder').each(function(index) {
 
-    var target = $(this).find('.current')
-    console.log(target.length);
+    var target = $(this).find('.current');
     setTimeout(function(){ flipCard(direction, target, deg, 'ease-in')}, timeout);
     timeout = timeout + 100;
   });
@@ -89,7 +111,7 @@ function flipCard(direction, target, deg, easing) {
   }); 
 }
 
-function transitionEndEvent(_this) {
+function flipTransitionEndEvent(_this) {
 
   if ($(_this).hasClass('next')) {
 
@@ -115,9 +137,68 @@ function transitionEndEvent(_this) {
   }
 }
 
-function setButtons() {
+//:::::::::::::::::::::::::::::::::::::::::::::::
+// ---------- IE 9
+//:::::::::::::::::::::::::::::::::::::::::::::::
 
-  $('.karhu.flip-card.btn.next, .karhu.flip-card.btn.prev').removeClass('hide');
-  if ($('.karhu.flip-card .current').next('.card').length == 0) { ($('.karhu.flip-card.btn.next').addClass('hide')); }
-  else if ($('.karhu.flip-card .current').prev('.card').length == 0) { ($('.karhu.flip-card.btn.prev').addClass('hide')); }
+function initIeNine() {
+
+  if ($(this).hasClass('preventClick')) { return; };
+
+  $('.karhu.flip-card.holder').each(function(index) {
+
+    $(this).find('.card').css({ 'opacity':'0' });
+    $(this).find('.card:first-child').css({ 'opacity':'1' }).addClass('current');
+  });
+
+  $('.karhu.flip-card.btn').on('click', function() {
+
+    if ($(this).hasClass('preventClick')) { return; };
+    if ($(this).hasClass('next')) {
+
+      $('.current').next('.card').addClass('revealed');
+      fadeCards();
+    }
+    else if ($(this).hasClass('prev')) {
+
+      $('.current').prev('.card').addClass('revealed');
+      fadeCards();
+    }
+
+    $('.karhu.flip-card.btn').addClass('preventClick');
+  });
+}
+
+function fadeCards() {
+
+  timeout = 0;
+  flipCount = 0;
+
+  $('.karhu.flip-card.holder').each(function(index) {
+
+    var target = $(this).find('.current')
+
+    $(target).delay(timeout).animate({
+
+      opacity: 0
+    }, 500, function() {
+
+      $(this).removeClass('current');
+      $(this).parent().find('.revealed').animate({
+
+        opacity: 1
+      }, 800, function() {
+
+        $(this).removeClass('revealed').addClass('current');
+        flipCount = flipCount + 1;
+        if(flipCount == $('.karhu.flip-card.holder').length) {
+
+          $('.karhu.flip-card.preventClick').removeClass('preventClick');
+          setButtons();
+        }
+      });
+    });
+
+    timeout = timeout + 100;
+  });
 }
